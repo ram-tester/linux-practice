b5b68f2e942722859e1f2d5d3337ac14cc66e7af
jaya369 <jayaram886@gmail.com>
2025-01-08 22:23:14 +0530

    added varibles example

--- 02-variables.sh
+++ 02-variables.sh
@@ -3,3 +3,12 @@ echo "A :: hello"
 echo "B :: hi"
 echo "A :: how are you?"
 echo "B :: am good"
+
+
+person1=ramesh
+person2=suresh
+
+echo  "$person1 :: Hi $person2"
+echo  "$person2 :: hello $person1"
+echo  "$person1 :: how are you $person2"
+echo "$person2 ::am good $person1"
