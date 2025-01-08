cc13f2874938a43fcb27e979ab74f8498e106df0
jaya369 <jayaram886@gmail.com>
2025-01-08 22:30:50 +0530

    added varibles example

---
+++ 02-varibales2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#### one more type of handling variables
+person3=$1
+person4=$2
+
+echo "$person3 :: Hi $person4"
+echo "$person4 :: hello $person3"
+echo "${person3} :: how are you $person4"
+echo "$person4 :: am good $person3"
