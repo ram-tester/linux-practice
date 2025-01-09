2740710dd5e43260d7e8a138817a1a491366c92f
jaya369 <jayaram886@gmail.com>
2025-01-09 16:54:52 +0530

    store logs in file

--- 11-logs.sh
+++ 11-logs.sh
@@ -10,8 +10,8 @@ Y="\e[33m"
 N="\e[0m"
 
 LOGS_FOLDER="/var/log/shellscript-logs"
-LOG_FILE=$(echo $0 |cut -d "." -f1)
-TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE=$( echo $0 | cut -d "." -f1 )
+TIMESTAMP=$( date +%Y-%m-%d-%H-%M-%S )
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
 VALIDATE()
@@ -25,29 +25,29 @@ if [ $1 -ne 0 ]
 fi
 }
 
-echo "script starts executed at $TIMESTAMP" &>>LOG_FILE_NAME
+echo "script starts executed at $TIMESTAMP" &>>$LOG_FILE_NAME
 if [ $USERID -ne 0 ]
 then
   echo "you must have sudo access execute this script"
   exit 1
 fi
 
-dnf list installed mysql  &>>LOG_FILE_NAME
+dnf list installed mysql  &>>$LOG_FILE_NAME
 #success status of last execution command status stores in $?
 if [ $? -ne 0 ]
 then
   echo "install mysql..."
-  dnf install mysql -y  &>>LOG_FILE_NAME
+  dnf install mysql -y  &>>$LOG_FILE_NAME
   VALIDATE  $? "mysql"
 else
   echo -e "mysql already $Y installed $N"
 fi
 
-dnf list installed git
+dnf list installed git  &>>$LOG_FILE_NAME
 if [ $? -ne 0 ]
 then
   echo "git installing now"
-  dnf install git -y  &>>LOG_FILE_NAME
+  dnf install git -y  &>>$LOG_FILE_NAME
   VALIDATE $? "GIT"
 else
   echo -e "git already $Y installed $N"
