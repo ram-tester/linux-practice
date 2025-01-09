dc8fc223cdaebd268cf9cae875c614a97b218548
jaya369 <jayaram886@gmail.com>
2025-01-09 11:06:36 +0530

    added array example

--- 04-data-type.sh
+++ 04-data-type.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 NUMBER1=$1
 NUMBER2=$2
-
 TIMESTAMP=$(date)
 echo "script executed at: $TIMESTAMP"
 SUM=$(($NUMBER1+$NUMBER2))
---
+++ 05-array.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+my_array=$('jai','jeevs')
+echo "first name:${my_array[0]}"
+echo "second name:${my_array[1]}"
+
+echo "list of names:${my_array[@]}"
