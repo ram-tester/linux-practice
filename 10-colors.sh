0aa778f811e050c5ba5aca0830d3ce47a40e8d23
jaya369 <jayaram886@gmail.com>
2025-01-09 15:43:07 +0530

    functions with colors

---
+++ 10-colors.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+VALIDATE()
+{
+if [ $1 -ne 0 ]
+  then
+    echo -e "$2 installation $R failed"
+    exit 1
+  else
+    echo -e "$G successfully installed $2"
+fi
+}
+if [ $USERID -ne 0]
+then
+  echo "you must have sudo access execute this script"
+  exit 1
+fi
+
+dnf list installed mysql
+#success status of last execution command status stores in $?
+if [ $? -ne 0 ]
+then
+  echo "install mysql..."
+  dnf install mysql -y
+  VALIDATE  $? "mysql"
+else
+  echo -e "mysql already $Y installed"
+fi
+
+dnf list installed git
+if [ $? -ne 0 ]
+then
+  echo "git installing now"
+  dnf install git -y
+  VALIDATE $? "GIT"
+else
+  echo "git already $Y installed "
+fi
