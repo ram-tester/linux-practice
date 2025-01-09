053cb2901dd342d3fe5f8c7ddd3aa38ea879bd09
jaya369 <jayaram886@gmail.com>
2025-01-09 20:53:39 +0530

    deleting old logs

---
+++ 14-delete-old-logs.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+SOURCE_DIR="/home/ec2-user/app-logs"
+
+
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME=$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log
+
+VALIDATE()
+{
+  if [ $1 -ne 0 ]
+  then
+    echo -e "$2 ... $R failure  $N"
+    exit 1
+  else
+    echo -e "$2... $G success   $N"
+  fi
+}
+
+
+CHECK_ROOT()
+{
+
+  if [ $USERID -ne 0 ]
+  then
+    echo -e "$R sudo user required to execute this script  $N"
+  exit 1
+  fi
+}
+
+
+echo "script started executing at : $TIMESTAMP"  &>>$LOG_FILE_NAME
+
+FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)
+echo "files to delete :: $FILES_TO_DELETE"
+
+
+while read -r filepath
+do
+  echo "deleting filepath : $filepath"  &>>LOG_FILE_NAME
+  rm -rf $filepath
+  echo "deleted file: $filepath"
+done <<< FILES_TO_DELETE
+
