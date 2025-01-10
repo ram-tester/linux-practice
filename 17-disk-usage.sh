582220cdf8d6906f7c3cfb827f2f575368dff94b
jaya369 <jayaram886@gmail.com>
2025-01-10 12:40:04 +0530

    disk usage with mail triggered

---
+++ 17-disk-usage.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT | grep xfs)  # reading file type xfs
+
+DISK_THRESHOLD=5
+
+MSG=""
+
+while read -r line   #reading line by line
+do
+  USAGE=$(echo $line| awk -F " " '{print $6F}'| cut -d "%" -f1) #1st fragment
+  PARTITION=$(echo $line | awk -F " " '{print $NF}')# last fragment
+  if [ $USAGE -ge $DISK_THRESHOLD ]
+  then
+    MSG +="High disk partition :$PARTITION usage is: $USAGE"
+  fi
+done <<<$DISK_USAGE
+
+echo -e "message :$MSG"
+
+echo "$MSG" | mutt -s "high disk usage" rampatropj@gmail.com
+
+
+
+
