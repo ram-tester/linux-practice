8c93678bb69058a8cc81680297e1f5a24b69fcb7
jaya369 <jayaram886@gmail.com>
2025-01-15 10:30:52 +0530

    for loop examples

---
+++ 20-for-loop-examples.sh
@@ -0,0 +1,16 @@
+NUMBER=$1
+res=0
+NUMBER1=$1
+for i in {1..NUMBER}
+do
+    rem=NUMBER%10
+    res=res+rem*rem*rem
+    NUMBER=NUMBER/10
+done
+echo $res
+if $res=$NUMBER1
+then
+    echo "given num is armstrong $res"
+else
+    echo "not a armstrong...$res"
+fi
