d71a453a6d5f3c7e9b10dc81af0511e5cf169b2b
jaya369 <jayaram886@gmail.com>
2025-01-16 11:35:55 +0530

    for loops examples

--- 20-for-loop-examples.sh
+++ 20-for-loop-examples.sh
@@ -1,12 +1,12 @@
 NUMBER=$1
 res=0
-NUMBER1=$1
-for i in {1..NUMBER}
+NUMBER1=$NUMBER
+while [ $NUMBER1 -ge 0 ]
 do
-    rem=$NUMBER%10
-    cube=$rem*$rem*$rem
-    res=$res+$cube
-    NUMBER=$NUMBER/10
+    rem=$((NUMBER%10))
+    cube=$((rem*rem*rem))
+    res=$((res+cube))
+    NUMBER=$((NUMBER/10))
 done
 echo $res
 if $res==$NUMBER1
