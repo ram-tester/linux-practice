09e3a7eb9903db2226ff33ba154059d290cb0814
jaya369 <jayaram886@gmail.com>
2025-01-16 12:44:38 +0530

    for loops examples

--- 20-for-loop-examples.sh
+++ 20-for-loop-examples.sh
@@ -1,12 +1,13 @@
 #!/bin/bash/
 echo "Enter A Number: "
 read -s n
+nod=${#n}
 arm=0
 num=$n
 while [ $n -gt 0 ]
 do
     r=$(($n % 10))
-    cube=$(($r*$r*$r))
+    cube=$(($r^$nod))
 	arm=$(($arm + $cube))
 	n=$(($n / 10))
 done
