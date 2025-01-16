3e1237e4101852df8c66a5247ea3bada6a5fb8cc
jaya369 <jayaram886@gmail.com>
2025-01-16 11:16:29 +0530

    for loops examples

--- 20-for-loop-examples.sh
+++ 20-for-loop-examples.sh
@@ -4,12 +4,12 @@ NUMBER1=$1
 for i in {1..NUMBER}
 do
     rem=NUMBER%10
-    cube=$(rem*rem*rem)
-    res=$(res+cube)
-    NUMBER=$(NUMBER/10)
+    cube=rem*rem*rem
+    res=res+cube
+    NUMBER=NUMBER/10
 done
 echo $res
-if $res=$NUMBER1
+if $res==$NUMBER1
 then
     echo "given num is armstrong $res"
 else
