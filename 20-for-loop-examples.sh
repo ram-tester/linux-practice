cbcb2516084283d0cb88a564d2a67c1cf909cf40
jaya369 <jayaram886@gmail.com>
2025-01-16 12:25:12 +0530

    for loops examples

--- 20-for-loop-examples.sh
+++ 20-for-loop-examples.sh
@@ -6,11 +6,11 @@ num=$n
 while [ $n -gt 0 ]
 do
     r=$(($n % 10))
-    cube=$((r*r*r))
-	arm=$((arm + cube))
+    cube=$(($r*$r*$r))
+	arm=$(($arm + $cube))
 	n=$(($n / 10))
 done
-echo $arm
+echo "after loop arm: $arm"
 if [ $arm -eq $num ]
 then
 	echo "given num is Armstrong :$num"
